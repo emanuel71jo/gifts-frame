b710717dbc9744bfdeded74c8502115d6e6d4d21
emanuel71jo <emanuel71jo@gmail.com>
2020-07-21 08:12:45 -0300

    Updated Readme

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+import app from "./app";
 
 app.listen(3333);
