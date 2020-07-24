89dc4fff85e1f7042a135a599a83d43f0064bb71
emanuel71jo <emanuel71jo@gmail.com>
2020-07-24 09:58:10 -0300

    Criação de festa

--- src/app/controllers/PartyController.ts
+++ src/app/controllers/PartyController.ts
@@ -1,18 +1,24 @@
 import { Response, Request } from 'express';
 import { getRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
+import { parseISO, isAfter } from 'date-fns';
 import Party from '@entities/Party';
 
 class PartyController {
   async store(request: Request, response: Response) {
     const repository = getRepository(Party);
 
-    const { name, party_date_at, userId } = request.body;
+    const { name, party_date_at } = request.body;
+
+    const isPartyPassAlready = isAfter(new Date(), parseISO(party_date_at));
+
+    if (isPartyPassAlready) {
+      return response.status(403).json({ error: 'Party has passed already' });
+    }
 
     const party = repository.create({
       name,
-      party_date_at: parseISO(party_date_at),
-      celebrant_id: userId,
+      partyDateAt: parseISO(party_date_at),
+      celebrantId: request.userId,
     });
 
     await repository.save(party);
